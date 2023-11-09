12aa118cec5f7fc9147a64563dfbd380a99563c3
Lívia Faria Guirão <118777022+liviaguirao@users.noreply.github.com>
2023-11-09 18:21:45 +0000

    Add files via upload

---
+++ TraineeRole1.h
@@ -0,0 +1,46 @@
+#ifndef RINO_PLAYER_H
+#define RINO_PLAYER_H
+
+#include "Role.h"
+
+using namespace std;
+
+class TraineeRole1: public Role {
+
+private:
+    bool onStart;
+    int timerKick;
+ 
+public:
+
+    float timerHead;
+    int direcao;
+    float distanciaParaChutar;
+    
+
+    int getTimerKick(){
+        return timerKick;
+    }
+
+    void attTimerKick(){
+        timerKick++;
+    }
+    
+    TraineeRole1(SpellBook *spellBook);
+    ~TraineeRole1();
+    void Tick(float ellapsedTime, const SensorValues&);
+    void penalti();
+    bool procura();
+    void centralizar();
+    bool ProcuraCentro();
+    bool ProcuraCentroEmbaixo();
+    bool ProcuraDireita();
+    bool ProcuraDireitaEmbaixo();
+    bool ProcuraEsquerda();
+    bool ProcuraEsquerdaEmbaixo();
+    void perseguir();
+    void chuta();
+
+
+};
+#endif
